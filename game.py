d8e15f181c12afa8dcd5df239e21c35f3fea4edc
Matthieu Bourgois <matthieubourgois@Matthieus-MBP.domain>
2018-10-03 20:53:32 -0700

    Cleaned up code

--- game.py
+++ game.py
@@ -7,10 +7,19 @@ global total_moves
 total_moves = []
 global valid_moves
 valid_moves = ['a1','a2','a3','b1','b2','b3','c1','c2','c3']
-global winning_combos
-winning_combos = [['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3'],['a1','b1','c1'],['a2','b2','c2'],['a3','b3','c3'],['a1','b2','c3'],['a3','b2','c1']]
 global moves
-moves = {'a1':' ','a2':' ','a3':' ','b1':' ','b2':' ','b3':' ','c1':' ','c2':' ','c3':' '}
+moves = {'a1':'','a2':'','a3':'','b1':'','b2':'','b3':'','c1':'','c2':'','c3':''}
+global winning_combos
+winning_combos = [
+[moves['a1'],moves['a2'],moves['a3']],
+[moves['b1'],moves['b2'],moves['b3']],
+[moves['c1'],moves['c2'],moves['c3']],
+[moves['a1'],moves['b1'],moves['c1']],
+[moves['a2'],moves['b2'],moves['c2']],
+[moves['a3'],moves['b3'],moves['c3']],
+[moves['a1'],moves['b2'],moves['c3']],
+[moves['a3'],moves['b2'],moves['c1']]]
+
 
 
 #functions
@@ -23,41 +32,45 @@ def check_for_win():
             elif player_Y_moves not in i:
                 print("Tie!")
                 return
-    for i in player_X_moves:
-        for z in winning_combos:
-            if i in z:
-                print(z)
-                
+    move_checker()
+
+def move_checker():
+    for x in winning_combos:
+        if x[0] in player_X_moves and x[1] in player_X_moves and x[2] in player_X_moves:
+            print("Player X Wins!")
+        if x[0] in player_Y_moves and x[1] in player_Y_moves and x[2] in player_Y_moves:
+            print("Player O Wins!")
+
 
 #Player X move
 def x_choice(): 
-    choice = str(input("Player X move (example b3): ")).lower()
-    if choice not in total_moves and choice in valid_moves:
-        player_X_moves.append(choice)
-        total_moves.append(choice)
-        plug_letters(choice)
-        check_for_win()
-        display_board()
-        y_choice()
-    else: 
-        print("Bogus move! Try again...")
-        return choice
+    choice = input("Player X move (example b3): ").lower()
+    while choice in total_moves or choice not in valid_moves:
+        choice = input("Bogus move! Try again...").lower()
+    player_X_moves.append(choice)
+    total_moves.append(choice)
+    plug_letters(choice)
+    print(player_X_moves)
+    check_for_win()
+    display_board()
+    y_choice()
+
 
 #Player Y move
 def y_choice():
-    choice = str(input("Player O move (example c2): ")).lower()
-    if choice not in total_moves and choice in valid_moves:        
-        player_Y_moves.append(choice)
-        total_moves.append(choice)
-        plug_letters(choice)
-        check_for_win()
-        display_board()
-        x_choice()
-    else: 
-        print("Bogus move! Try again...")
-        return choice    
+    choice = input("Player O move (example c2): ").lower()
+    while choice in total_moves or choice not in valid_moves:
+        choice = input("Bogus move! Try again...").lower()
+    player_Y_moves.append(choice)
+    total_moves.append(choice)
+    plug_letters(choice)
+    print(player_Y_moves)
+    check_for_win()
+    display_board()
+    x_choice()
 
-# adds letters to 
+
+# adds letters to board
 def plug_letters(t):
     if t in player_X_moves:
         moves[t] = 'X'
@@ -67,13 +80,15 @@ def plug_letters(t):
 #board
 def display_board():
     print(f"""
-    A   B   C
+        A   B   C
 
-    1) {moves['a1']}| {moves['b1']} |{moves['c1']}
-    -----------
-    2) {moves['a2']}| {moves['b2']} |{moves['c2']}
-    -----------
-    3) {moves['a3']}| {moves['b3']} |{moves['c3']}""")
+    1)   {moves['a1']} |   {moves['b1']} | {moves['c1']}
+    -------------------
+    2)   {moves['a2']} |   {moves['b2']} | {moves['c2']}
+    -------------------
+    3)   {moves['a3']} |   {moves['b3']} | {moves['c3']}
+    
+                                                    """)
     
 
 #Calling of functions
